e3c3685f2796e921b53d95834b6dc7fb61ffca0b
Eero Gallano <gallanoeero@gmail.com>
2026-02-23 20:18:58 -0800

    CELEBRATION

--- app/call/page.tsx
+++ app/call/page.tsx
@@ -8,9 +8,38 @@ import {
   GridLayout,
   ParticipantTile,
   VideoConference,
+  useRoomContext,
 } from "@livekit/components-react";
-import { useEffect, useState } from "react";
+import { RoomEvent } from "livekit-client";
+import { useCallback, useEffect, useState } from "react";
 import { generate } from 'yet-another-name-generator'
+import confetti from "canvas-confetti";
+
+function JoinEffect() {
+  const room = useRoomContext();
+
+  const fireJoinAnimation = useCallback(() => {
+    // Play join sound
+    const audio = new Audio("/sounds/join.mp3");
+    audio.play().catch(() => {});
+
+    // Fire confetti
+    confetti({
+      particleCount: 150,
+      spread: 80,
+      origin: { y: 0.6 },
+    });
+  }, []);
+
+  useEffect(() => {
+    room.on(RoomEvent.ParticipantConnected, fireJoinAnimation);
+    return () => {
+      room.off(RoomEvent.ParticipantConnected, fireJoinAnimation);
+    };
+  }, [room, fireJoinAnimation]);
+
+  return null;
+}
 
 export default function RoomPage() {
 
@@ -49,6 +78,7 @@ export default function RoomPage() {
       video
       style={{ height: "90vh" }}
     >
+      <JoinEffect />
       {/* <RoomAudioRenderer />
       <GridLayout tracks={[]}>
         <ParticipantTile />
